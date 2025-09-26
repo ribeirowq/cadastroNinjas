73914f1d31ca10a28cf8570a4cdafe3b05b349d1
Thiago Ribeiro <ribeiro7380@gmail.com>
2025-09-26 17:31:42 -0300

    fix: Corrige posição de entidade o Long no JPA Repository

---
+++ cadastroNinjas/src/main/java/dev/javath/cadastroNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.javath.cadastroNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
