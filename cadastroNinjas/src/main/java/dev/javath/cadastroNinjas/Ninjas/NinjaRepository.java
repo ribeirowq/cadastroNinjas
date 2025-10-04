d83d1069d95eaae73a90990770486f8a27f29ba6
Thiago Ribeiro <ribeiro7380@gmail.com>
2025-10-04 01:03:04 -0300

    fix: Inclue metodo findByMissoes no repositorio ninja

--- cadastroNinjas/src/main/java/dev/javath/cadastroNinjas/Ninjas/NinjaRepository.java
+++ cadastroNinjas/src/main/java/dev/javath/cadastroNinjas/Ninjas/NinjaRepository.java
@@ -1,6 +1,10 @@
 package dev.javath.cadastroNinjas.Ninjas;
 
+import dev.javath.cadastroNinjas.Missoes.MissoesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+    List<NinjaModel> findByMissoes(MissoesModel missoes);
 }
